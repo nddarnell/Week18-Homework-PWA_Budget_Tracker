6955063f81066874c796c2cadec98e5a487c7471
Nathan Darnell <darnell.nathaniel95@gmail.com>
2020-07-08 17:14:48 -0700

    added on success and on error in db.js

--- public/db.js
+++ public/db.js
@@ -2,3 +2,13 @@ let db;
 
 const request = indexedDB.open("budget", 1);
 
+request.onsuccess = (event)=>{
+    db = event.target.result
+    if(navigator.onLine){
+        checkDB()
+    }
+}
+
+request.onerror = (event)=>{
+  console.log(event.target.errorCode);
+}
